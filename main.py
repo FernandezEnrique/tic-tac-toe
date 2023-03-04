cd3d48051d0de2d9b15b27961717bc98a7bf42ce
Enrique Fernández <fernandezvelascoenrique@gmail.com>
2023-03-04 13:24:23 +0100

    Colors file and updated main.py with the main class

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import pygame
+from pygame.locals import *
+from colors import *
 
+class Game:
 
-pygame.init()
-
-pygame.display.set_caption('Quick Start')
-window_surface = pygame.display.set_mode((800, 600))
-
-background = pygame.Surface((800, 600))
-background.fill(pygame.Color('#000000'))
-
-is_running = True
-
-while is_running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            is_running = False
-
-    window_surface.blit(background, (0, 0))
-
-    pygame.display.update()
+    def __init__(self):
+        pygame.init()
+        flags = RESIZABLE
+        self.screen = pygame.display.set_mode((600, 600), flags)
+        self.running = True
+        self.background = WHITE
+        self.screen.fill(self.background)
+        pygame.display.set_caption('Tic Tac Toe')
+        pygame.display.update()
+    
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False       
+        pygame.quit()
+        
+        
+    
+if __name__ == '__main__':
+    Game().run()
